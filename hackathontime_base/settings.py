8a48c4c92aa959a897f9f25decdc6447a156fec6
nk521 <nk_mason@protonmail.com>
2019-12-14 01:19:48 +0530

    asd

--- hackathontime_base/settings.py
+++ hackathontime_base/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJ_DEBUG')
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'www.hackathonti.me', 'hackathonti-me.herokuapp.com']
 
