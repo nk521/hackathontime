a94a94fb5628d276eab583c98f696af31759196f
nk521 <nk_mason@protonmail.com>
2019-12-14 00:53:13 +0530

    added django_heroku

--- hackathontime_base/settings.py
+++ hackathontime_base/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -144,3 +145,5 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL=os.environ.get('DEFAULT_FROM_EMAIL')
+
+django_heroku.settings(locals())
